278659fea42a8bf296c0c962d8e5ff8d4a59e455
Sdrapinski <szymondrapinski@gmail.com>
2022-09-09 16:47:01 +0200

    first two contracts

---
+++ deploy/02-deploy-random-ipfs.js
@@ -0,0 +1,27 @@
+const { network, ethers } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  let vrfCoordinatorv2Address, subscriptionId;
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      "VRFCoordinatorV2Mock"
+    );
+    vrfCoordinatorv2Address = vrfCoordinatorV2Mock.address;
+    const tx = await vrfCoordinatorV2Mock.createSubscription();
+    const txReceipt = await tx.wait(1);
+    subscriptionId = txReceipt.events[0].args.subId;
+  } else {
+    vrfCoordinatorv2Address = networkConfig[chainId].vrfCoordinatorV2;
+    subscriptionId = networkConfig[chainId].subscriptionId;
+  }
+};
